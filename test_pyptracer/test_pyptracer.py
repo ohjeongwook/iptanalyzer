c55d479297c80d8c7edf2edde1919824e3af2a46
Jeong Wook Oh <oh.jeongwook@gmail.com>
2019-10-28 11:58:43 -0700

    Added trace fille extraction code

--- test_pyptracer/test_pyptracer.py
+++ test_pyptracer/test_pyptracer.py
@@ -1,9 +1,15 @@
 import os
 import sys
-
 sys.path.append(r'..\x64\Debug')
 
+from zipfile import ZipFile
+
 import pyptracer
+
+if not os.path.isfile('trace.pt'):
+    print("* Extracting test trace file:")
+    with ZipFile('trace.zip', 'r') as zf:
+       zf.extractall()
 
 p=pyptracer.PTracerLib()
 p.Open(r'trace.pt')
