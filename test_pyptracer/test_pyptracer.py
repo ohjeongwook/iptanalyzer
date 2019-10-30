a0fc2be74f8ef9ebd59f517ab20e39db79800207
Jeong Wook Oh <oh.jeongwook@gmail.com>
2019-10-29 17:14:10 -0700

    Added symbol dump mode

--- test_pyptracer/test_pyptracer.py
+++ test_pyptracer/test_pyptracer.py
@@ -9,12 +9,19 @@ import capstone
 import pyptracer
 import windbgtool.debugger
 
-class PyTracer:
-    def __init__(self, pt_zip_filename, pt_filename, dump_filename = ''):
+class PyPTracer:
+    def __init__(self, pt_zip_filename, pt_filename, dump_filename = '', dump_instructions = False, dump_symbols = True, disassembler = "capstone"):
+        self.DumpInstructions = dump_instructions
+        self.DumpSymbols = dump_symbols
+        self.Disassembler = disassembler
+
         if dump_filename:
             self.Debugger = windbgtool.debugger.DbgEngine()
             #self.Debugger.SetLogLevel(True)
             self.Debugger.LoadDump(dump_filename)
+
+            if dump_symbols:
+                self.Debugger.EnumerateModules()
 
         self._ExtractTracePT(pt_zip_filename, pt_filename)
 
@@ -32,9 +39,24 @@ class PyTracer:
         for byte in raw_bytes:
             raw_line += '%.2x ' % (byte % 256)
 
+    def LoadMemory(self, ip):
+        address_info = self.Debugger.GetAddressInfo(ip)
+
+        if self.DumpSymbols:
+            for (address, symbol) in self.Debugger.EnumerateModuleSymbols([address_info['Module Name'], ]).items():
+                self.AddressToSymbols[address] = symbol
+
+        base_address = int(address_info['Base Address'], 16)
+        region_size = int(address_info['Region Size'], 16)
+
+        dmp_filename = '%x.dmp' % base_address
+        self.Debugger.RunCmd('.writemem %s %x L?%x' % (dmp_filename, base_address, region_size))
+        self.PyTracer.AddImage(base_address, dmp_filename)
+
     def Run(self):
         self.PyTracer.StartInstructionDecoding()
 
+        self.AddressToSymbols = {}
         move_forward = True
         i = 0
         while 1:
@@ -42,26 +64,35 @@ class PyTracer:
             if not insn:
                 break
 
-            raw_bytes = insn.GetRawBytes()
-            md = capstone.Cs(capstone.CS_ARCH_X86, capstone.CS_MODE_64)
-            for disas in md.disasm(bytearray(raw_bytes), insn.ip):        
-                print('%x: %s %s' % (disas.address, disas.mnemonic, disas.op_str))
-                break
+            if self.DumpInstructions or (self.DumpSymbols and insn.ip in self.AddressToSymbols):
+                symbol = ''
+                if insn.ip in self.AddressToSymbols:
+                    symbol = self.AddressToSymbols[insn.ip]
+
+                if self.Disassembler == "capstone":
+                    raw_bytes = insn.GetRawBytes()
+                    md = capstone.Cs(capstone.CS_ARCH_X86, capstone.CS_MODE_64)
+                    for disas in md.disasm(bytearray(raw_bytes), insn.ip):                   
+                        print('%s (%x): %s %s' % (symbol, disas.address, disas.mnemonic, disas.op_str))
+                        break
+
+                elif self.Disassembler == "windbg":
+                        disasmline = self.Debugger.RunCmd('u %x L1' % (insn.ip))
+                        print('\t'+disasmline)
 
             errcode = self.PyTracer.GetNextInsnStatus()
-            if errcode == pyptracer.pt_error_code.pte_nomap:
-                address_info = self.Debugger.GetAddressInfo(insn.ip)
-                base_address = int(address_info['Base Address'], 16)
-                region_size = int(address_info['Region Size'], 16)
-                dmp_filename = '%x.dmp' % base_address
-                self.Debugger.RunCmd('.writemem %s %x L?%x' % (dmp_filename, base_address, region_size))
-                self.PyTracer.AddImage(base_address, dmp_filename)
-                move_forward = False
-                continue
+            if errcode != pyptracer.pt_error_code.pte_ok:
+                print('errorcode= %s' % errcode)
+
+                if errcode == pyptracer.pt_error_code.pte_nomap:
+                
+                    self.LoadMemory(insn.ip)
+                    move_forward = False
+                    continue
 
             i += 1
             move_forward = True
 
 if __name__ == '__main__':
-    pytracer = PyTracer('../TestFiles/trace.zip', '../TestFiles/trace.pt', '../TestFiles/notepad.exe.dmp')
+    pytracer = PyPTracer('../TestFiles/trace.zip', '../TestFiles/trace.pt', '../TestFiles/notepad.exe.dmp')
     pytracer.Run()
