67ad2c8c09d9185b1409c44122299b0aad664353
Jeong Wook Oh <oh.jeongwook@gmail.com>
2019-10-30 16:34:45 -0700

    Added block decode test code

--- RunPTracer/main.cpp
+++ RunPTracer/main.cpp
@@ -14,16 +14,23 @@ void main(int argc, char* argv[]) {
     filename = argv[1];
 
     PTracer ptracer = PTracer();
-    ptracer.Open(filename);
-    ptracer.AddImage(0x00007ffbb5ba1000, "..\\TestFiles\\00007ffb`b5ba1000.dmp");
-    ptracer.AddImage(0x00007ffbb7cc1000, "..\\TestFiles\\00007ffb`b7cc1000.dmp");
-
-    ptracer.InitDecoding();
+    ptracer.Open(filename, 1208096168);
+    // ptracer.AddImage(0x00007ffbb5ba1000, "..\\TestFiles\\00007ffb`b5ba1000.dmp");
+    // ptracer.AddImage(0x00007ffbb7cc1000, "..\\TestFiles\\00007ffb`b7cc1000.dmp");
 
     for (;;) {
-        struct pt_insn* insn = ptracer.DecodeInstruction();
+        struct pt_block* p_block = ptracer.DecodeBlock();
 
-        if (ptracer.GetStatus() == -pte_eos)
+        if (!p_block)
+        {
+            break;
+        }
+    }
+
+    for (;;) {
+        struct pt_insn* p_insn = ptracer.DecodeInstruction();
+
+        if (!p_insn)
         {
             break;
         }
