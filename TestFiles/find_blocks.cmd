6e98500359d38e3598fdf8ef45d2b029da0d44d9
Jeong Wook Oh <oh.jeongwook@gmail.com>
2019-11-01 08:27:45 -0700

    Added test scripts

---
+++ TestFiles/find_blocks.cmd
@@ -0,0 +1,2 @@
+python ..\pyptracertool\find_blocks.py -c Cache -p trace.pt -d notepad.exe.dmp -s "KERNELBASE!CreateFileW"
+pause
---
+++ TestFiles/generate_block_cache.cmd
@@ -0,0 +1,2 @@
+python ..\pyptracertool\generate_block_cache.py -p trace.pt -d notepad.exe.dmp
+pause
