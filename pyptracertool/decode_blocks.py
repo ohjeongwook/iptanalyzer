ace538fc28a409708224780831c1aab65aa9cf81
Jeong Wook Oh <oh.jeongwook@gmail.com>
2019-10-31 18:17:08 -0700

    Added sync offset/offset test case

--- pyptracertool/decode_blocks.py
+++ pyptracertool/decode_blocks.py
@@ -15,14 +15,16 @@ if __name__ == '__main__':
     cache_folder = 'Tmp'
     pt_filename = '../TestFiles/trace.pt'
     dump_filename = '../TestFiles/notepad.exe.dmp'
-    start_offset = 0x283d2178 - 1024
-    end_offset = start_offset + 1024*20
+    start_offset = 0x13aba74
+    end_offset = start_offset + 0x20cd + 10
 
     pytracer = decoder.PTLogAnalyzer(pt_filename, 
                                      dump_filename, 
-                                     dump_symbols = True, 
+                                     dump_symbols = False, 
                                      load_image = True, 
                                      start_offset = start_offset,
-                                     end_offset = end_offset)
-    pytracer.DecodeBlock()
+                                     end_offset = end_offset,
+                                     disassembler = "windbg")
 
+    for block in pytracer.DecodeBlock(move_forward = False, block_offset = 0x13ada69):
+        print('block.ip: %x ~ %x (%d)' % (block.ip, block.end_ip, block.ninsn))
--- pyptracertool/decode_instructions.py
+++ pyptracertool/decode_instructions.py
@@ -16,13 +16,18 @@ if __name__ == '__main__':
     cache_folder = 'Tmp'
     pt_filename = '../TestFiles/trace.pt'
     dump_filename = '../TestFiles/notepad.exe.dmp'
-    start_offset = 0x283d2178 - 1024*5
-    end_offset = start_offset + 1024*10
+    start_offset = 0x13aba74
+    end_offset = start_offset + 0x20cd + 10
 
-    pytracer = decoder.PTLogAnalyzer(pt_filename, 
-                                     dump_filename, 
-                                     dump_symbols = True, 
-                                     load_image = True, 
+    pytracer = decoder.PTLogAnalyzer(pt_filename,
+                                     dump_filename,
+                                     dump_symbols = False,
+                                     dump_instructions = False,
+                                     load_image = True,
                                      start_offset = start_offset,
-                                     end_offset = end_offset)
-    pytracer.DecodeInstruction(move_forward = False)
+                                     end_offset = end_offset,
+                                     disassembler = "windbg")
+
+    for insn in pytracer.DecodeInstruction(move_forward = False, instruction_offset = 0x13ada69):
+        disasmline = pytracer.GetDisasmLine(insn)
+        print('Instruction: %s' % (disasmline))
