6f01d24ea2361033c3ab34ae82a81c81d002f736
Jeong Wook Oh <oh.jeongwook@gmail.com>
2019-11-10 12:18:00 -0800

    Changed default offset value for ptracer.Open

--- PTracerTest/main.cpp
+++ PTracerTest/main.cpp
@@ -14,7 +14,7 @@ void main(int argc, char* argv[]) {
     filename = argv[1];
 
     PTracer ptracer = PTracer();
-    ptracer.Open(filename, 1208096168);
+    ptracer.Open(filename, 0);
     // ptracer.AddImage(0x00007ffbb5ba1000, "..\\TestFiles\\00007ffb`b5ba1000.dmp");
     // ptracer.AddImage(0x00007ffbb7cc1000, "..\\TestFiles\\00007ffb`b7cc1000.dmp");
 
