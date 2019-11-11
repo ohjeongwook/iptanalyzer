931b359e43ff4169b7ddc1f290598980705cc0ab
Jeong Wook Oh <oh.jeongwook@gmail.com>
2019-11-11 10:06:33 -0800

    Changed PTLogAnalyzer options

--- pyptracertool/decode_blocks.py
+++ pyptracertool/decode_blocks.py
@@ -18,21 +18,28 @@ if __name__ == '__main__':
         return int(x, 0)
 
     parser = argparse.ArgumentParser(description='PyPTracer')
-    parser.add_argument('-p', action = "store", dest = "pt")
-    parser.add_argument('-d', action = "store", dest = "dump")
+    parser.add_argument('-p', action = "store", default = "", dest = "pt")
+    parser.add_argument('-d', action = "store", default = "", dest = "dump")
     parser.add_argument('-s', dest = "start_offset", default = 0, type = auto_int)
     parser.add_argument('-e', dest = "end_offset", default = 0, type = auto_int)
     parser.add_argument('-b', dest = "block_offset", default = 0, type = auto_int)
 
     args = parser.parse_args()
 
+    if args.dump:
+        dump_symbols = True
+        load_image = True
+    else:
+        dump_symbols = False
+        load_image = False
+
     pytracer = decoder.PTLogAnalyzer(args.pt, args.dump, 
-                                     dump_symbols = False, 
-                                     load_image = True, 
+                                     dump_symbols = dump_symbols, 
+                                     load_image = load_image, 
                                      start_offset = args.start_offset,
                                      end_offset = args.end_offset,
                                      disassembler = "windbg")
 
     for block in pytracer.DecodeBlock(move_forward = False, block_offset = args.block_offset):
-        print('block.ip: %x ~ %x (%d)' % (block.ip, block.end_ip, block.ninsn))
+        print('block.ip: %.16x ~ %.16x (%.16x)' % (block.ip, block.end_ip, block.ninsn))
 
