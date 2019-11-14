7f5c3d76515997d6a14d723611bb0c91e1f72fde
Jeong Wook Oh <oh.jeongwook@gmail.com>
2019-11-14 15:35:27 -0800

    Changed script names

--- TestFiles/decode_blocks.cmd
+++ TestFiles/decode_blocks.cmd
@@ -1,2 +1,2 @@
-python ..\pyptracertool\decode_blocks.py -p trace.pt -d notepad.exe.dmp -S 0x13aba74 -E 0x13adb4f -b 0x13ada69 
+python ..\pyptracertool\enumerate_blocks.py -p trace.pt -d notepad.exe.dmp -S 0x13aba74 -E 0x13adb4f -b 0x13ada69 
 pause
