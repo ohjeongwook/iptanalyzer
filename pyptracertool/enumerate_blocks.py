4dc7efd999611cee4602c8a5a53b2fbee72f6afa
Jeong Wook Oh <oh.jeongwook@gmail.com>
2019-11-14 20:50:36 -0800

    Merged list_blocks to enumerate_blocks

--- pyptracertool/enumerate_blocks.py
+++ pyptracertool/enumerate_blocks.py
@@ -26,6 +26,9 @@ if __name__ == '__main__':
     parser.add_argument('-E', dest = "end_offset", default = 0, type = auto_int)
 
     parser.add_argument('-b', dest = "block_offset", default = 0, type = auto_int)
+    
+    parser.add_argument('-c', action = "store", dest = "cache")
+    parser.add_argument('-C', dest = "cr3", default = 0, type = auto_int)    
 
     args = parser.parse_args()
 
@@ -36,11 +39,21 @@ if __name__ == '__main__':
         dump_symbols = False
         load_image = False
 
-    ptlog_analyzer = ipt.LogAnalyzer(args.dump, 
-                                     dump_symbols = dump_symbols, 
-                                     load_image = load_image)
+    if args.cache:
+        block_analyzer = block.CacheReader(args.cache, args.pt)
+        dump_loader = dump.Loader(args.dump)
 
-    ptlog_analyzer.OpenPTLog(args.pt, start_offset = args.start_offset, end_offset = args.end_offset)
+        for (sync_offset, offset, address) in block_analyzer.EnumerateBlockRange(cr3 = args.cr3, start_address = args.start_address, end_address = args.end_address):
+            symbol = dump_loader.GetSymbol(address)
+            print('> %.16x (%s) (sync_offset=%x, offset=%x)' % (address, symbol, sync_offset, offset))
+            disasm_line = dump_loader.GetDisasmLine(address)
+            print('\t' + disasm_line)
+    else:
+        ptlog_analyzer = ipt.LogAnalyzer(args.dump, 
+                                         dump_symbols = dump_symbols, 
+                                         load_image = load_image)
 
-    for block in ptlog_analyzer.EnumerateBlocks(move_forward = False, block_offset = args.block_offset):
-        print('block.ip: %.16x ~ %.16x (%.16x)' % (block.ip, block.end_ip, block.ninsn))
+        ptlog_analyzer.OpenPTLog(args.pt, start_offset = args.start_offset, end_offset = args.end_offset)
+
+        for block in ptlog_analyzer.EnumerateBlocks(move_forward = False, block_offset = args.block_offset):
+            print('block.ip: %.16x ~ %.16x (%.16x)' % (block.ip, block.end_ip, block.ninsn))
