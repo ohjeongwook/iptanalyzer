8b310ba49dcba94f784f1897d6b807a8053eb814
Jeong Wook Oh <oh.jeongwook@gmail.com>
2019-11-22 14:53:14 +0900

    Added setup.py

---
+++ pyptracertool/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyptracertool-ohjeongwook",
+    version="0.0.1",
+    author="Matt Oh",
+    author_email="jeongoh@darungrim.com",
+    description="WinDbg Tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ohjeongwook/PTracer",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.7',
+)
