cb1a70252442b726a6283023fbd2b3fa28ea7d12
Matt Oh <oh.jeongwook@gmail.com>
2020-07-01 20:50:48 -0700

    Code refactored Added single cpu mode for debugging

--- pyipttool/generate_cache.py
+++ pyipttool/generate_cache.py
@@ -23,7 +23,28 @@ def set_log_file(filename):
         log.removeHandler(hdlr)
     log.addHandler(fh)
 
-def decode_blocks_function(pt_filename, dump_filename, queue, temp_foldername, log_directory):
+def decode_block(pt_filename, dump_filename, temp_directory, cache_filename, start_offset = 0, end_offset = 0):
+    logging.debug("decode_block: dump_filename: %s, cache_filename: %s" % (dump_filename, cache_filename))
+    pt_log_analyzer = pyipttool.ipt.Analyzer(dump_filename, dump_symbols = False, load_image = True, temp_directory = temp_directory)
+    pt_log_analyzer.open_ipt_log(pt_filename, start_offset = start_offset, end_offset = end_offset)
+
+    try:
+        logging.debug("# pt_log_analyzer.record_block_offsets")
+        pt_log_analyzer.record_block_offsets()
+    except:
+        tb = traceback.format_exc()
+        logging.debug("# decode_blocks_function exception: %s" % str(tb))
+
+    logging.debug("# decode_blocks_function: Writing %.16x ~ %.16x to %s" % (start_offset, end_offset, cache_filename))
+    if cache_filename:
+        try:
+            cache_writer = pyipttool.cache.Writer(pt_log_analyzer.basic_block_addresss_to_offsets, pt_log_analyzer.block_offsets_to_ips)
+            cache_writer.save(cache_filename)
+        except:
+            tb = traceback.format_exc()
+            logging.debug("# decode_blocks_function WriteBlockOffsets Exception: %s" % tb)
+
+def decode_blocks_function(pt_filename, dump_filename, queue, temp_directory, log_directory):
     log_filename = os.path.join(log_directory, str(uuid.uuid1()) + '.log')
     logging.basicConfig(level=logging.DEBUG, filename = log_filename, filemode = 'w', format = '%(name)s - %(levelname)s - %(message)s')
 
@@ -38,24 +59,7 @@ def decode_blocks_function(pt_filename, dump_filename, queue, temp_foldername, l
         set_log_file(decode_blocks_function_log_filename)
 
         logging.debug("# decode_blocks_function: %.16x ~ %.16x" % (start_offset, end_offset))
-
-        pt_log_analyzer = pyipttool.ipt.Analyzer(dump_filename, dump_symbols = False, load_image = True, temp_foldername = temp_foldername)
-        pt_log_analyzer.open_ipt_log(pt_filename, start_offset = start_offset, end_offset = end_offset)
-
-        try:
-            pt_log_analyzer.decode_blocks()
-        except:
-            tb = traceback.format_exc()
-            logging.debug("# decode_blocks_function DecodeBlocks Exception: %s" % tb)
-
-        logging.debug("# decode_blocks_function: Writing %.16x ~ %.16x to %s" % (start_offset, end_offset, cache_filename))
-        if cache_filename:
-            try:
-                cache_writer = pyipttool.cache.Writer(pt_log_analyzer.basic_block_addresss_to_offsets, pt_log_analyzer.block_offsets_to_ips)
-                cache_writer.save(cache_filename)
-            except:
-                tb = traceback.format_exc()
-                logging.debug("# decode_blocks_function WriteBlockOffsets Exception: %s" % tb)
+        decode_block(pt_filename, dump_filename, temp_directory, cache_filename, start_offset, end_offset)
 
 if __name__ == '__main__':
     import argparse
@@ -68,9 +72,9 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='pyipt')
     parser.add_argument('-p', action = "store", default = "", dest = "pt_filename")
-    parser.add_argument('-d', action = "store", default = "", dest = "dump_file")
-    parser.add_argument('-o', action = "store", default="blocks.cache", dest = "cache_file")
-    parser.add_argument('-t', action = "store", default = tempfile.gettempdir(), dest = "temp")
+    parser.add_argument('-d', action = "store", default = "", dest = "dump_filename")
+    parser.add_argument('-o', action = "store", default="blocks.cache", dest = "cache_filename")
+    parser.add_argument('-t', action = "store", default = tempfile.gettempdir(), dest = "temp_directory")
     parser.add_argument('-l', action = "store", default = os.path.join(os.getcwd(), "logs"), dest = "log_directory")
     parser.add_argument('-O', dest = "offset", default = 0, type = auto_int)
 
@@ -82,48 +86,60 @@ if __name__ == '__main__':
         except:
             traceback.print_exc()
 
-    ipt_analyzer = pyipttool.ipt.Analyzer(args.dump_file, dump_symbols = False, load_image = False, progress_report_interval = 100)
-    ipt_analyzer.open_ipt_log(args.pt_filename, start_offset = 0)
-    sync_offsets = ipt_analyzer.enumerate_sync_offsets()
+    use_multiprocess = True
 
-    import multiprocessing
+    if not use_multiprocess:
+        logging.basicConfig(
+            level=logging.DEBUG,
+            format = '%(name)s - %(levelname)s - %(message)s',
+            handlers = [
+                #logging.FileHandler(os.path.join(args.log_directory, str(uuid.uuid1()) + '.log')),
+                logging.StreamHandler()
+        ])
+        decode_block(args.pt_filename, args.dump_filename, args.temp_directory, args.cache_filename)
+    else:
+        import multiprocessing
 
-    process_count = multiprocessing.cpu_count()
-    pqueue = multiprocessing.Queue()
+        ipt_analyzer = pyipttool.ipt.Analyzer(args.dump_filename, dump_symbols = False, load_image = False, progress_report_interval = 100)
+        ipt_analyzer.open_ipt_log(args.pt_filename, start_offset = 0)
+        sync_offsets = ipt_analyzer.enumerate_sync_offsets()
 
-    print("Launching decode block functions...")
-    procs = []
-    cache_filenames = []
+        process_count = multiprocessing.cpu_count()
+        pqueue = multiprocessing.Queue()
 
-    for i in range(0, process_count, 1):
-        proc = multiprocessing.Process(target = decode_blocks_function, args=(args.pt_filename, args.dump_file, pqueue, args.temp, args.log_directory))
-        procs.append(proc)
-        proc.start()
+        print("Launching decode block functions...")
+        procs = []
+        cache_filenames = []
 
-    offsets_count = len(sync_offsets)
-    chunk_size = int(offsets_count / process_count)
-    for start_index in range(0, offsets_count, chunk_size):
-        end_index = start_index + chunk_size
-        if end_index < offsets_count:
-            start_offset = sync_offsets[start_index]
-            end_offset = sync_offsets[end_index]
-        else:
-            start_offset = sync_offsets[start_index]
-            end_offset = 0
+        for i in range(0, process_count, 1):
+            proc = multiprocessing.Process(target = decode_blocks_function, args=(args.pt_filename, args.dump_filename, pqueue, args.temp_directory, args.log_directory))
+            procs.append(proc)
+            proc.start()
 
-        cache_filename = os.path.join(args.temp, 'block-%.16x-%.16x.cache' % (start_offset, end_offset))
-        cache_filenames.append(cache_filename)
-        pqueue.put((start_offset, end_offset, cache_filename))
+        offsets_count = len(sync_offsets)
+        chunk_size = int(offsets_count / process_count)
+        for start_index in range(0, offsets_count, chunk_size):
+            end_index = start_index + chunk_size
+            if end_index < offsets_count:
+                start_offset = sync_offsets[start_index]
+                end_offset = sync_offsets[end_index]
+            else:
+                start_offset = sync_offsets[start_index]
+                end_offset = 0
 
-    for i in range(0, process_count, 1):
-        pqueue.put(None)
+            cache_filename = os.path.join(args.temp_directory, 'block-%.16x-%.16x.cache' % (start_offset, end_offset))
+            cache_filenames.append(cache_filename)
+            pqueue.put((start_offset, end_offset, cache_filename))
 
-    for proc in procs:
-        proc.join()
+        for i in range(0, process_count, 1):
+            pqueue.put(None)
 
-    print("Merging block cache files...")
-    merger = pyipttool.cache.Merger()
-    for filename in cache_filenames:
-        merger.read(filename)
-        os.unlink(filename)
-    merger.write(args.cache_file)
+        for proc in procs:
+            proc.join()
+
+        print("Merging block cache files...")
+        merger = pyipttool.cache.Merger()
+        for filename in cache_filenames:
+            merger.read(filename)
+            os.unlink(filename)
+        merger.write(args.cache_filename)
