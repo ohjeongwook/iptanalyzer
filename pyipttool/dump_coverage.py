244ae825c27957a5896b9eecb2097f4a6f5d5d7b
Matt Oh <oh.jeongwook@gmail.com>
2020-07-04 01:13:15 -0700

    Separated dump_coverage from dump_blocks

--- pyipttool/dump_coverage.py
+++ pyipttool/dump_coverage.py
@@ -25,30 +25,18 @@ if __name__ == '__main__':
     parser.add_argument('-d', action = "store", default = "", dest = "dump_filename")
 
     parser.add_argument('-m', action = "store", dest = "module_name", default = "")
-    parser.add_argument('-o', action = "store", dest = "output_filename", default = "output.log")
+    parser.add_argument('-o', action = "store", dest = "output_filename", default = "coverage.txt")
     
     parser.add_argument('-D', dest = "debug_level", default = 0, type = auto_int)
     parser.add_argument('-O', action = "store", dest = "debug_filename", default = "stdout")
-
+    
     parser.add_argument('-s', dest = "start_address", default = 0, type = auto_int)
     parser.add_argument('-e', dest = "end_address", default = 0, type = auto_int)
-
-    parser.add_argument('-S', dest = "start_offset", default = 0, type = auto_int)
-    parser.add_argument('-E', dest = "end_offset", default = 0, type = auto_int)
-
-    parser.add_argument('-b', dest = "block_offset", default = 0, type = auto_int)
     
     parser.add_argument('-c', action = "store", dest = "cache_file")
     parser.add_argument('-C', dest = "cr3", default = 0, type = auto_int)    
 
     args = parser.parse_args()
-
-    if args.dump_filename:
-        dump_symbols = True
-        load_image = True
-    else:
-        dump_symbols = False
-        load_image = False
 
     if args.debug_level > 0:
         handlers = []
@@ -83,15 +71,9 @@ if __name__ == '__main__':
         coverage_logger = pyipttool.coverage.Logger(module_name, start_address, end_address, args.pt_filename, args.dump_filename, debugger = debugger)
         
         for (offset, address, end_address, sync_offset) in block_analyzer.enumerate_block_range(cr3 = args.cr3, start_address = start_address, end_address = end_address):
-            symbol = debugger.find_symbol(address)
-            print('> %.16x (%s) (sync_offset=%x, offset=%x)' % (address, symbol, sync_offset, offset))
-            print('\t' + debugger.get_disassembly_line(address))
-    else:
-        ptlog_analyzer = pyipttool.ipt.Analyzer(args.dump_filename, 
-                                         dump_symbols = dump_symbols, 
-                                         load_image = load_image,
-                                         debug_level = args.debug_level)
+            coverage_logger.add_block(offset, address, end_address, sync_offset)
 
-        ptlog_analyzer.open_ipt_log(args.pt_filename, start_offset = args.start_offset, end_offset = args.end_offset)
-        for block in ptlog_analyzer.decode_blocks(offset = args.block_offset, start_address = start_address, end_address = end_address):
-            print('block.ip: %.16x ~ %.16x (%.16x)' % (block.ip, block.end_ip, block.ninsn))
+        if args.output_filename:
+            coverage_logger.save(args.output_filename)
+        else:
+            coverage_logger.print()
